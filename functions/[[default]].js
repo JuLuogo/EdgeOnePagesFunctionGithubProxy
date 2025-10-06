ba498084c2e092e056107b877e3c949d5468b90e
JuLuogo <1576586736@qq.com>
2025-10-06 21:52:43 +0800

    fix: 将主机名前缀检查从'X.'改为'x.'以保持一致性
    修改了getProxyPrefix函数和modifyResponse函数中对主机名前缀的检查，从大写'X.'改为小写'x.'，确保整个代码库中前缀检查的一致性。

--- functions/[[default]].js
+++ functions/[[default]].js
@@ -126,8 +126,8 @@ new_response_headers.delete('content-length');
 // 获取当前主机名的前缀，用于匹配反向映射
 function getProxyPrefix(host) {
   // 检查主机名是否以 X. 开头
-  if (host.startsWith('X.')) {
-    return 'X.';
+  if (host.startsWith('x.')) {
+    return 'x.';
   }
   
   // 检查其他映射前缀
@@ -172,7 +172,7 @@ async function modifyResponse(response, host_prefix, effective_hostname) {
   }
 
   // 处理相对路径，使用有效主机名
-  if (host_prefix === 'X.') {
+  if (host_prefix === 'x.') {
     text = text.replace(
       /(?<=["'])\/(?!\/|[a-zA-Z]+:)/g,
       `https://${effective_hostname}/`
